9bf705308614cec18c7a0ece17359ae82de35138
LeonardSSH <leonardssh22@gmail.com>
2020-12-04 00:29:00 +0100

    feat: add support for emojis

--- main.py
+++ main.py
@@ -14,6 +14,141 @@ import json
 with open("config.json", "r") as f:
     config = json.load(f)
 
+# https://openweathermap.org/weather-conditions
+weather_codes = {
+    # Code - Icon
+    "200": "11",
+    "201": "11",
+    "202": "11",
+    "210": "11",
+    "211": "11",
+    "212": "11",
+    "221": "11",
+    "230": "11",
+    "231": "11",
+    "232": "11",
+    "300": "09",
+    "301": "09",
+    "302": "09",
+    "310": "09",
+    "311": "09",
+    "312": "09",
+    "313": "09",
+    "314": "09",
+    "321": "09",
+    "500": "10",
+    "501": "10",
+    "502": "10",
+    "503": "10",
+    "504": "10",
+    "511": "13",
+    "520": "09",
+    "521": "09",
+    "522": "09",
+    "531": "09",
+    "600": "13",
+    "601": "13",
+    "602": "13",
+    "611": "13",
+    "612": "13",
+    "613": "13",
+    "615": "13",
+    "616": "13",
+    "620": "13",
+    "621": "13",
+    "622": "13",
+    "701": "50",
+    "711": "50",
+    "721": "50",
+    "731": "50",
+    "741": "50",
+    "751": "50",
+    "761": "50",
+    "762": "50",
+    "771": "50",
+    "781": "50",
+    "800": "01",
+    "801": "02",
+    "802": "03",
+    "803": "04",
+    "804": "04"
+}
+
+weather_emojis = {
+    # d > day | n > night
+    "01": {
+        "d": {
+            "emoji_name": "☀️"
+        },
+        "n": {
+            "emoji_name": "🌑"
+        }
+    },
+    "02": {
+        "d": {
+            "emoji_name": "🌥️"
+        },
+        "n": {
+            "emoji_name": "☁"
+        }
+    },
+    "03": {
+        "d": {
+            "emoji_name": "☁"
+        },
+        "n": {
+            "emoji_name": "☁"
+        }
+    },
+    "04": {
+        "d": {
+            "emoji_name": "☁"
+        },
+        "n": {
+            "emoji_name": "☁"
+        }
+    },
+    "09": {
+        "d": {
+            "emoji_name": "🌧️"
+        },
+        "n": {
+            "emoji_name": "🌧️"
+        }
+    },
+    "10": {
+        "d": {
+            "emoji_name": "🌧️"
+        },
+        "n": {
+            "emoji_name": "🌧️"
+        }
+    },
+    "11": {
+        "d": {
+            "emoji_name": "🌩️"
+        },
+        "n": {
+            "emoji_name": "🌩️"
+        }
+    },
+    "13": {
+        "d": {
+            "emoji_name": "❄️"
+        },
+        "n": {
+            "emoji_name": "❄️"
+        }
+    },
+    "50": {
+        "d": {
+            "emoji_name": "🌫️"
+        },
+        "n": {
+            "emoji_name": "🌫️"
+        }
+    },
+}
 
 def update_bio(text):
     """
@@ -85,9 +220,13 @@ def generate_bio_content(weather):
     feels_like = round(weather["main"]["feels_like"])
     city = config["city"]
 
+    # Get the weather emoji
+    weather_emoji = resolve_icon(weather)
+
     # Returns the final string wich contain the city, the current temp, the felt temp, the weather and the last update
     return (
-        "Current weather in "
+        weather_emoji
+        + " Current weather in "
         + city
         + ": "
         + str(temp)
@@ -103,6 +242,24 @@ def generate_bio_content(weather):
         + " | Made using Python"
     )
 
+def resolve_icon(weather):
+    """
+    Resolve the weather icon
+    Args:
+        weather (str): The weather object
+    Returns:
+        emoji (str): The emoji name
+    """
+    weather_id = str(weather["weather"][0]["id"])
+    day_or_night = str(weather["weather"][0]["icon"][2:])
+
+    emoji = '☁'
+
+    if weather_id in weather_codes:
+        if weather_codes[weather_id] in weather_emojis:
+            emoji = weather_emojis[weather_codes[weather_id]][day_or_night]['emoji_name']
+
+    return emoji
 
 def main():
     """
